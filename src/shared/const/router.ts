cfd8d1f3e49b8f2fa83fb886ef4cddd9b6aed476
Andrey Chipizubov <andryukha.chipizybov@mail.ru>
2023-05-24 08:26:50 +0300

    fix

--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -7,3 +7,30 @@ export enum AppRouters {
 
 export const getRouteMain = () => '/';
 export const getRouteForbidden = () => '/forbidden';
+
+// export const RoutePath: Record<AppRoutes, string> = {
+//     [AppRoutes.MAIN]: '/',
+//     [AppRoutes.FORBIDDEN]: '/forbidden',
+
+//     [AppRoutes.NOT_FOUND]: '*',
+// };
+
+// const onBackToList = useCallback(() => {
+//     navigate(RoutePath.articles);
+// }, [navigate]);
+
+// const onEdit = useCallback(() => {
+//     navigate(`${RoutePath.details}${article?.id}/edit`);
+// }, [article?.id, navigate]);
+
+// export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
+
+// const onBackToList = useCallback(() => {
+//     navigate(getRouteArticles());
+// }, [navigate]);
+
+// const onEditArticle = useCallback(() => {
+//     if (article) {
+//         navigate(getRouteArticleEdit(article.id));
+//     }
+// }, [article, navigate]);
